01a1d3d0e37c7f81a04da2e9707ac1c39f4642b9
Marcus Furlong <furlongm@gmail.com>
2019-10-14 18:00:26 +0100

    don't use SETGROUPS on macos

--- src/util/virutil.c
+++ src/util/virutil.c
@@ -1042,7 +1042,7 @@ virSetUIDGID(uid_t uid, gid_t gid, gid_t *groups ATTRIBUTE_UNUSED,
         return -1;
     }
 
-# if HAVE_SETGROUPS
+# if HAVE_SETGROUPS && !defined(__APPLE__)
     if (gid != (gid_t)-1 && setgroups(ngroups, groups) < 0) {
         virReportSystemError(errno, "%s",
                              _("cannot set supplemental groups"));
